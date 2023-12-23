d795ba5467aae67169e50360a03a7bbba2f48aaf
Koushik Chandrasekaran <69519007+CK-ghub@users.noreply.github.com>
2023-12-23 12:32:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function typeWriter(element, text, i, callback) {
+    if (i < text.length) {
+        element.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(function() {
+            typeWriter(element, text, i, callback);
+        }, 150); // Typing speed in milliseconds
+    } else if (typeof callback == 'function') {
+        callback();
+    }
+}
+
+function startTypingEffect() {
+    const paragraphs = document.getElementsByTagName('p');
+    let i = 0;
+    function typeNextParagraph() {
+        if (i < paragraphs.length) {
+            paragraphs[i].style.display = ''; // Make paragraph visible
+            typeWriter(paragraphs[i], paragraphs[i].dataset.text, 0, typeNextParagraph);
+            i++;
+        }
+    }
+    typeNextParagraph();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    startTypingEffect();
+});
